7e89bb02a8b7e8b2f2fe17851a22c3a700f1d580
Samar khan <166284907+QtvsNewOffi@users.noreply.github.com>
2024-11-22 08:32:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,11 +38,11 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 USERNAME = environ.get("USERNAME", "https://t.me/Itsking_45")
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '885675538 6074403072').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '885675538 6074403072 5937730372').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002124824105').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '885675538 6074403072').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '885675538 6074403072 5937730372').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '885675538 6074403072').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '885675538 6074403072 5937730372').split()]
 auth_channel = environ.get('AUTH_CHANNEL', '-1002124824105')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
